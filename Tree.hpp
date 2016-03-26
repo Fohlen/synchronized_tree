6e92daa362a6ce9c623c5df9218d7504963f93f8
Fohlen <lennard@berger.work>
2016-03-26 18:48:44 +0100

    Added empty tree for test

---
+++ Tree.hpp
@@ -0,0 +1,19 @@
+/*
+ * Tree.hpp
+ *
+ */
+
+#ifndef TREE_HPP_
+#define TREE_HPP_
+
+// This can be extended in later steps but we're currently fine using the default
+#include <boost/property_tree/ptree.hpp>
+
+namespace inexor {
+namespace tree {
+	boost::property_tree::ptree Tree;
+}
+}
+
+
+#endif /* TREE_HPP_ */
